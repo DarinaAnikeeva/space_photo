7c9062c2aaf370fc3761da5886070de4e471587f
DarinaAnikeeva <anikeevadarina29@gmail.com>
2022-06-25 16:14:53 +0300

    publish photo to Telegram channel

--- telegrem_bot.py
+++ telegrem_bot.py
@@ -2,4 +2,5 @@ import telegram
 
 bot = telegram.Bot(token='5003644672:AAE_0V7VikR4A9sueeZPuP8ptME2enm5hR0')
 chat_id = "@fabio_bot_1"
-bot.send_message(text='Hi John!', chat_id=chat_id)
+bot.send_message(text='Hi!', chat_id=chat_id)
+bot.send_document(chat_id=chat_id, document=open('monro.jpg', 'rb'))
