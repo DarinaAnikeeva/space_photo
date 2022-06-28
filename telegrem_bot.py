fc62a39b4364660b2dc9198ff1fbf7b048d5a067
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-06-28 15:31:15 +0300

    Update telegrem_bot.py

--- telegrem_bot.py
+++ telegrem_bot.py
@@ -5,45 +5,91 @@ import random
 import time
 import sys
 
-from fetch_spacex_launch import fetch_spacex_launch
-from apod_photos import apod_photos
-from epic_photos import epic_photos
+from fetch_spacex_launch import get_fetch_spacex_launch
+from APOD_photos import get_apod_photos
+from EPIC_photos import get_epic_photos
 from dotenv import load_dotenv
 
 
-def full(images_path, count_photos_apod, count_photos_epic, api_key, flight_number):
-    apod_photos(images_path, count_photos_apod, api_key)
-    epic_photos(images_path, count_photos_epic, api_key)
-    fetch_spacex_launch(images_path, flight_number)
+def combines_functions(images_path, 
+                       count_photos_apod, 
+                       count_photos_epic, 
+                       api_key, flight_number):
+    get_apod_photos(images_path, count_photos_apod, api_key)
+    get_epic_photos(images_path, count_photos_epic, api_key)
+    get_fetch_spacex_launch(images_path, flight_number)
 
 
-def get_images(images_path, count_photos_apod, count_photos_epic, api_key, time_sleep, flight_number=None,
+def get_images(images_path, 
+               count_photos_apod, 
+               count_photos_epic, 
+               api_key, 
+               time_sleep, 
+               flight_number=None,
                user_image=None):
-    if user_image == None:
-        if not os.path.exists(images_path):
-            full(images_path, count_photos_apod, count_photos_epic, api_key, flight_number)
-        images = os.listdir(f'{images_path}/')
-        while images:
-            random_image = random.choice(images)
-            bot.send_document(chat_id=chat_id, document=open(f'{images_path}/{random_image}', 'rb'))
-            images.remove(random_image)
-            time.sleep(time_sleep)
-        else:
-            full(images_path, count_photos_apod, count_photos_epic, api_key, flight_number)
-            get_images(images_path, count_photos_apod, count_photos_epic, api_key, time_sleep, flight_number=None)
-    else:
+    if user_image:
         bot.send_document(chat_id=chat_id, document=open(f'{user_image}', 'rb'))
         time.sleep(time_sleep)
-        get_images(images_path, count_photos_apod, count_photos_epic, api_key, time_sleep, flight_number=None)
+        get_images(images_path, 
+                   count_photos_apod, 
+                   count_photos_epic, 
+                   api_key, 
+                   time_sleep, 
+                   flight_number=None)
+    else:
+        none_user_image(images_path, 
+                        count_photos_apod, 
+                        count_photos_epic, 
+                        api_key, 
+                        flight_number, 
+                        time_sleep)
+        get_images(images_path, 
+                   count_photos_apod, 
+                   count_photos_epic, 
+                   api_key, 
+                   time_sleep, 
+                   flight_number=None)
+
+
+def none_user_image(images_path, 
+                    count_photos_apod, 
+                    count_photos_epic, 
+                    api_key, 
+                    flight_number, 
+                    time_sleep):
+    if not os.path.exists(images_path):
+        combines_functions(images_path, 
+                           count_photos_apod, 
+                           count_photos_epic, 
+                           api_key, 
+                           flight_number)
+    images = os.listdir(f'{images_path}/')
+    while images:
+        random_image = random.choice(images)
+        bot.send_document(chat_id=chat_id, document=open(f'{images_path}/{random_image}', 'rb'))
+        images.remove(random_image)
+        time.sleep(time_sleep)
+    else:
+        combines_functions(images_path, 
+                           count_photos_apod, 
+                           count_photos_epic, 
+                           api_key, 
+                           flight_number)
 
 
 if __name__ == '__main__':
     load_dotenv()
     api_key = os.environ['NASA_TOKEN']
     bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
-    chat_id = "@fabio_bot_1"
+    chat_id = os.environ['TG_CHAT_ID']
+
     parser = argparse.ArgumentParser()
     parser.add_argument('timer', nargs='?', default=20)
     namespace = parser.parse_args(sys.argv[1:])
-    get_images(images_path='images', count_photos_apod=2, count_photos_epic=1, api_key=api_key,
-               time_sleep=namespace.timer)
+
+    get_images(images_path='images', 
+               count_photos_apod=2, 
+               count_photos_epic=1, 
+               api_key=api_key,
+               time_sleep=namespace.timer, 
+               flight_number=108)
